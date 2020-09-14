a7040f6a7c024ea5d34c1b6a6852fa30c4763eec
collozy <ozlocollins@gmail.com>
2020-09-14 16:46:50 +0300

    Modified the bottom and side navigation UI to look better.

--- app/src/main/java/com/misolova/medifora/ui/MainActivity.kt
+++ app/src/main/java/com/misolova/medifora/ui/MainActivity.kt
@@ -4,9 +4,13 @@ import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.core.view.isVisible
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -18,68 +22,55 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, BottomNavigationView.OnNavigationItemSelectedListener {
 
-    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var drawerAppBarConfiguration: AppBarConfiguration
     private lateinit var listener: NavController.OnDestinationChangedListener
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val navController = findNavController(R.id.my_nav_host_fragment)
-        appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.homeFragment,
-            R.id.answerRequestFragment,
-            R.id.profileFragment
-        ))
-        drawerAppBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        appBarConfiguration = AppBarConfiguration.Builder(R.id.homeFragment, R.id.answerRequestFragment,
+            R.id.profileFragment)
+            .setOpenableLayout(drawerLayout)
+            .build()
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-
-        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout,
-            R.string.start,
-            R.string.close
-        )
-        drawerLayout.addDrawerListener(actionBarDrawerToggle)
-        actionBarDrawerToggle.syncState()
         navigation_view.setupWithNavController(navController)
         navigation_view.setNavigationItemSelectedListener(this)
 
-
         bottom_navigation_view.setupWithNavController(navController)
-        bottom_navigation_view.setOnNavigationItemSelectedListener(this)
+        bottom_navigation_view.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.homeMenuItem -> findNavController(
+                    R.id.my_nav_host_fragment
+                ).navigate(R.id.homeFragment)
+                R.id.answerRequestsMenuItem -> findNavController(
+                    R.id.my_nav_host_fragment
+                ).navigate(R.id.answerRequestFragment)
+                R.id.userProfileMenuItem -> findNavController(
+                    R.id.my_nav_host_fragment
+                ).navigate(R.id.profileFragment)
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
 
-
-        listener = NavController.OnDestinationChangedListener{controller, destination, arguments ->
+        listener = NavController.OnDestinationChangedListener{_, destination, _ ->
             when(destination.id){
-                R.id.homeFragment -> supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(
-                    R.color.colorPrimaryDark
-                )))
-                R.id.profileFragment -> supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(
-                    R.color.colorPrimary
-                )))
-                R.id.answerRequestFragment -> supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(
-                    R.color.colorAccent
-                )))
+                R.id.homeFragment, R.id.profileFragment, R.id.answerRequestFragment -> {
+                    bottom_navigation_view.visibility = View.VISIBLE
+                }
+                else -> bottom_navigation_view.visibility = View.GONE
             }
         }
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.my_nav_host_fragment)
-        return navController.navigateUp(drawerAppBarConfiguration) || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
-            return true
-        }
-        return true
-    }
-
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
@@ -105,6 +96,15 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 R.id.my_nav_host_fragment
             ).navigate(R.id.profileFragment)
         }
+        drawerLayout.closeDrawer(GravityCompat.START, false)
         return super.onSupportNavigateUp()
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
